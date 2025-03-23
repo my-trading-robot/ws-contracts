8ba18c9b9e0948cf0711963c81ba90cd45fb073a
amigin <amigin@gmail.com>
2025-03-23 23:00:14 +0200

    Added BidAsk

--- src/bid_ask_ws_model.rs
+++ src/bid_ask_ws_model.rs
@@ -1,7 +1,7 @@
 use serde::*;
 
 #[derive(Serialize, Deserialize)]
-pub struct BidAskModel {
+pub struct BidAskWsModel {
     pub id: String,
     pub bid: f64,
     pub ask: f64,
--- src/ws_contract.rs
+++ src/ws_contract.rs
@@ -4,10 +4,12 @@ use super::*;
 
 const REQUEST_CANDLES: &'static str = "candles-req";
 const RESPONSE_CANDLES: &'static str = "candles-res";
+const BID_ASK: &'static str = "bid-ask";
 
 pub enum WsContract {
     GetCandlesRequest(GetCandlesWsRequestContract),
     GetCandlesResponse(GetCandlesWsResponseContract),
+    BidAsk(Vec<BidAskWsModel>),
 }
 
 impl WsContract {
@@ -27,6 +29,10 @@ impl WsContract {
                 let data = serde_json::from_str(payload).unwrap();
                 return Some(Self::GetCandlesResponse(data));
             }
+            BID_ASK => {
+                let data = serde_json::from_str(payload).unwrap();
+                return Some(Self::BidAsk(data));
+            }
             _ => return None,
         }
     }
@@ -43,6 +49,13 @@ impl WsContract {
                 let mut result = serde_json::to_string(data).unwrap();
                 result.insert(0, ':');
                 result.insert_str(0, REQUEST_CANDLES);
+                result
+            }
+
+            WsContract::BidAsk(data) => {
+                let mut result = serde_json::to_string(data).unwrap();
+                result.insert(0, ':');
+                result.insert_str(0, BID_ASK);
                 result
             }
         };
