a073a3b4dfb7ea21ecac595e646a82dba0c9af68
amigin <amigin@gmail.com>
2025-03-25 19:00:19 +0200

    Minor Improvements

--- src/set_active_instrument_model.rs
+++ src/set_active_instrument_model.rs
@@ -1,8 +1,16 @@
 use serde::{Deserialize, Serialize};
 
+use crate::WsContract;
+
 //id - empty - means global
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct SetActiveInstrumentWsModel {
     pub id: String,
+}
+
+impl Into<WsContract> for SetActiveInstrumentWsModel {
+    fn into(self) -> WsContract {
+        WsContract::SetActiveInstrument(self)
+    }
 }
