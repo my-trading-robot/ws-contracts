929dff56a85c004aff541d003a48b30c25f2420e
amigin <amigin@gmail.com>
2025-03-25 16:16:05 +0200

    Minor fixes

--- src/error_ws_model.rs
+++ src/error_ws_model.rs
@@ -1,6 +1,14 @@
 use serde::*;
 
+use crate::WsContract;
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct ErrorWsModel {
     pub msg: String,
+}
+
+impl Into<WsContract> for ErrorWsModel {
+    fn into(self) -> WsContract {
+        WsContract::Error(self)
+    }
 }
