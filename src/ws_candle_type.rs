2eea12733bfe2af2398711083f2bcbbaa7393d1f
amigin <amigin@gmail.com>
2025-03-26 12:38:54 +0200

    Added support of Min5 Candles

--- src/ws_candle_type.rs
+++ src/ws_candle_type.rs
@@ -1,6 +1,7 @@
 #[derive(Debug, Clone, Copy)]
 pub enum WsCandleType {
     Minute,
+    Min5,
     Hour,
     Day,
     Month,
@@ -10,6 +11,7 @@ impl WsCandleType {
     pub fn as_str(&self) -> &str {
         match self {
             WsCandleType::Minute => crate::consts::MINUTE,
+            WsCandleType::Min5 => crate::consts::MIN_5,
             WsCandleType::Hour => crate::consts::HOUR,
             WsCandleType::Day => crate::consts::DAY,
             WsCandleType::Month => crate::consts::MONTH,
@@ -19,6 +21,7 @@ impl WsCandleType {
     pub fn from_str(src: &str) -> Self {
         match src {
             crate::consts::MINUTE => Self::Minute,
+            crate::consts::MIN_5 => Self::Min5,
             crate::consts::HOUR => Self::Hour,
             crate::consts::DAY => Self::Day,
             crate::consts::MONTH => Self::Month,
